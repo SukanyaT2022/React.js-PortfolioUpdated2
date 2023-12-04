f4499bd9c841e4dd2d4f2146ee89b11e838d8d96
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-12-04 06:33:52 -0500

    add

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
 
         {/* text side */}
         <div className='text-aboutme w-full md:w-[60%]  md:pr-36 md:pl-10 md:pt-24 md:pb-16 px-10'>
-<h2 className='font-bold md:text-2xl text-xl pb-5 text-[#565656]'>Who I am & What Inspire me</h2>
+<h2 className='font-bold md:text-2xl text-xl pb-5 text-[#565656]'>Who am I ? & What inspires me</h2>
 <p className='text-md text-[#565656]'>
 My background is in business analysis and investment, but I have always been curious about learning software development. I took a break from work for six years to raise my two children. Once my children started school, I had more time to pursue my real interest.
 
@@ -24,7 +24,7 @@ I first learned JavaScript from Free Code Camp, Udemy, and YouTube. Specifically
 
 After three months of self-teaching, I knew what I would love to do for my future career. I then registered at Norwalk Community College as a Computer Science major and put all my effort into building my expertise through many school and personal projects. I look forward to many opportunities to use my skills in the exciting field of software development.
 </p>
-<button className='bg-[#565656] text-white px-10 py-3 rounded-3xl my-7'>Contact Me</button>
+<button className='bg-[#565656] text-white px-10 py-3 rounded-3xl my-7 md:ml-3 block m-auto hover:bg-[#0088cc]'>Contact Me</button>
         </div>
 
       </div>
--- src/Contact.js
+++ src/Contact.js
@@ -5,84 +5,43 @@ const Contact = () => {
   return (
     <div>
 
-      <h2>Contact</h2>
-      <div className='wrapper'>
-      <div className='contactForm'>
+      <h2 className="targetTitle text-center py-10 text-3xl md:text-5xl font-bold">Contact Us</h2>
 
+    <div className='mainBox flex md:justify-evenly justify-center flex-col md:flex-row  w-full px-4 md:mb-10'>
 
-<section class="bg-white dark:bg-gray-900">
-  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-     
-      <form  action="https://formspree.io/f/xayvrwlo" method="post" class="space-y-8">
+      <form  action="https://formspree.io/f/xayvrwlo" method="post" className="space-y-8 md:w-[40%] w-full pt-5">
       <div>
-              <label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your First Name</label>
-              <input type="text" name="Fname" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="First Name" required/>
+              {/* <label for="Name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your First Name</label> */}
+              <input type="text" name="Fname" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="First Name" required/>
           </div>
           <div>
-              <label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Last Name</label>
-              <input type="text" name="Lname" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+              
+              <input type="text" name="Lname" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
               placeholder="Last Name" required/>
           </div>
           <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
+              
+              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
           </div>
         
-          <div class="sm:col-span-2">
-              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-              <textarea name="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+          <div className="sm:col-span-2">
+             
+              <textarea name="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave your message..."></textarea>
           </div>
-          {/* <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button> */}
-          <button type="submit" class="py-3 px-5 bg-red-300">Test button</button>
+          {/* <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button> */}
+          <button type="submit" className='bg-[#565656] text-white px-10 py-3 rounded-3xl my-7 md:ml-3 block m-auto hover:bg-[#0088cc]'>Submit</button>
       </form>
-{/* og */}
 
-  {/* <div class="parent">
-    <div class="row">
-      <div class="col-lg-5 leftSide">
-  
+ {/* contact info side */}
+ <div className='md:w-[40%] w-full bg-purple-300'>
+  <h2>info</h2>
+</div>
 
-        <div class="contact2">
-          <img src="contact2-removebg-preview (1).png">
-        </div>
-
-        <form action="https://formspree.io/f/xayvrwlo" method="post">
-
-          <label for="Name">First Name</label>
-          <label for="Name" class="lastName">Last Name</label><br>
-
-          <!--<label for="Name" class="lastName">Last Name</label><br>-->
-          <!--to move lastname lable we add class lastName to target-->
-          <!--br tag help firstname on top of the box-->
-          <input type="text" name="Fname" required>
-
-          <input type="text" name="Lname" required><br>
-          <!--email-->
-          <label for="email">Email</label><br>
-          <input type="email" name="email" required>
-
-          <!--message-->
-          <br>
-          <label for="text">Message</label><br>
-          <textarea name="Message" id="" cols="35" rows="8"></textarea><br>
-          <!--we can make box bigger or smaller by changing number cols (width)and row(height)-->
-          <!--submit-->
-          <input type="submit" value="Send Message">
-        </form>
-  */}
-
-
-
-{/* og */}
   </div>
-</section>
+ 
       </div>
-      <div className='info'>
-<p>info</p>
-      </div>
-      </div>
-    </div>
+
+ 
   )
 }
 
