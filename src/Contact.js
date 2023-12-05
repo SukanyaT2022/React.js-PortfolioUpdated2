acc0bb7c556098592cbc94136de9b29c30d6125a
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-12-05 08:42:40 -0500

    add

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Contact.css'
+import { MdPhoneIphone } from "react-icons/md";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { GrLinkedin } from "react-icons/gr";
 
 const Contact = () => {
   return (
@@ -21,7 +25,7 @@ const Contact = () => {
           </div>
           <div>
               
-              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
+              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Email" required/>
           </div>
         
           <div className="sm:col-span-2">
@@ -33,8 +37,13 @@ const Contact = () => {
       </form>
 
  {/* contact info side */}
- <div className='md:w-[40%] w-full bg-purple-300'>
-  <h2>info</h2>
+ <div className='md:w-[40%] w-full md:h-[360px] h-auto mt-[20px] bg-gray-200  text-md pl-14 pt-14'>
+  <h2 className='font-bold text-xl'>Contact Information</h2>
+  <a href='tel:+13474419148' ><h2 className='flex items-center my-3'><span className='mx-4'><MdPhoneIphone/></span>+13474419148</h2></a>
+  <a href='mailto:SukanyaT2022A@gmail.com' target='_blank'><h2 className='flex items-center my-3'><span className='mx-4'><MdOutlineMailOutline/></span>SukanyaT2022A@gmail.com</h2></a>
+  <a href='https://github.com/SukanyaT2022?tab=repositories' target='_blank'><h2 className='flex items-center my-3'><span className='mx-4'><FaGithub /></span>https://github.com/SukanyaT2022</h2></a>
+  <a href='https://www.linkedin.com/in/tiparpron-sukanya/' target='_blank'><h2 className='flex items-center my-3'><span className='mx-4'><GrLinkedin/></span>
+https://www.linkedin.com/in/tiparpron-sukanya/</h2></a>
 </div>
 
   </div>
