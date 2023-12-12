7d373adfc0b005f05ca8961b4cd6f356d42e3cbd
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-12-12 05:46:04 -0500

    add

--- src/Home.js
+++ src/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
       </div>
     <div className='text'>
       <p>I have a passion for creating scalable front-end products that deliver exceptional user experiences. Crafting digital solutions that seamlessly blend aesthetics and functionality is where I thrive.</p>
-      <a href ="contact"><button className='bg-[#F17a7e] py-2 px-5 rounded-3xl lg:mt-2 mt-5'>Contact Me</button></a>
+      <a href ="contact"><button className='bg-[#F17a7e] py-2 px-5 rounded-3xl lg:mt-5 mt-5 '>Contact Me</button></a>
       {/* lg means ipad air- md ipad mini - sm is phone */}
       </div>
   
